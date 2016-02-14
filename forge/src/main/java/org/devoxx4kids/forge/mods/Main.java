f5de0484a281401bb8f542c22907b7734116433b
Animcogn <corbanvilla@gmail.com>
2016-02-14 16:24:34 -0700

    Finished V1

--- forge/src/main/java/org/devoxx4kids/forge/mods/Main.java
+++ forge/src/main/java/org/devoxx4kids/forge/mods/Main.java
@@ -1,6 +1,8 @@
 package org.devoxx4kids.forge.mods;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -18,8 +20,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Main {
 
-	public static final String MODID = "myMods";
-	public static final String VERSION = "1.0";
+	public static final String MODID = "ImprovedMinecraft";
+	public static final String VERSION = "1.1";
 	public static Block enderBlock;
 	public static Item enderIngot;
 	public static Item enderEye;
@@ -79,6 +81,25 @@ public class Main {
 				1.0F);
 		Items.cake.setPotionEffect(PotionHelper.blazePowderEffect
 				+ "+6" + "+14");
+		
+		Item enderBlockItem = GameRegistry.findItem("mymods", "enderBlock"); 
+		ModelResourceLocation enderBlockModel =
+			new ModelResourceLocation("mymods:enderBlock", "inventory"); 
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+			.register(enderBlockItem, 0, enderBlockModel); 
+
+		Item enderIngotItem = GameRegistry.findItem("mymods", "enderIngot"); 
+		ModelResourceLocation enderIngotModel =
+			new ModelResourceLocation("mymods:enderIngot", "inventory");
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+			.register(enderIngotItem, 0, enderIngotModel);
+		
+		Item enderEyeItem = GameRegistry.findItem("mymods", "enderEye");
+		ModelResourceLocation enderEyeModel =
+			new ModelResourceLocation("mymods:enderEye", "inventory");
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+			.register(enderEyeItem, 0, enderEyeModel);
+		
 	}
 	@EventHandler
 	public void registerCommands(FMLServerStartingEvent event) {
